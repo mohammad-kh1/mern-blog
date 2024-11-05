89b6f00bf90d70fdeaab102e386854c99db9ad1d
Mohammad <D@gmail.com>
2024-11-04 23:14:41 -0800

    USERS MODEL

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,1 +1,43 @@
 import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const UsersSchema = new Schema({
+
+	ID:{
+		tpye:mongoose.Schema.Types.ObjectId,
+	    default: () => new mongoose.Types.ObjectId(),
+	},
+
+	firstName : {
+		type:String,
+	},
+	lastName:{
+		type:String,
+	},
+	email:{
+		type:String,
+		required:true,
+		unique:true,
+		lowercase:true,
+		trim:true,
+	},
+	password:{
+		type:String,
+		required:true,
+	},
+	date:{
+		type:Date,
+		default: Date.now
+	},
+	username:{
+		type:String,
+		required:String,
+
+	}
+
+
+});
+
+const User = mongoose.model("Users" , UsersSchema);
+
+module.export = User;
